426fc1bf0dda9ff7a606b0650f5fdfc7f322f4cf
Harald Kreuzer <mail@haraldkreuzer.net>
2023-01-06 23:28:41 +0100

    add alerts widgets

---
+++ alert_widget.py
@@ -0,0 +1,35 @@
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+from kivy.utils import get_color_from_hex
+
+
+class AlertWidget(ScrollView):
+    weather_data = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__()
+
+        self.label = Label(font_size=16, color=get_color_from_hex('#2E2E2E'))
+        self.add_widget(self.label)
+        self.bind(pos=self.update_text_position)
+        self.bind(size=self.update_text_position)
+
+        Clock.schedule_interval(self.update_text_position, 1 / 10)
+
+    def on_weather_data(self, instance, value):
+        alerts_list = []
+        alerts = self.weather_data.alerts
+        if alerts is not None:
+            for alert in self.weather_data.alerts:
+                alerts_list.append(alert.event + ": " + alert.description)
+            self.label.text = ' '.join(alerts_list)
+        else:
+            self.label.text = ""
+
+    def update_text_position(self, *args):
+        self.label.x -= 1
+
+        if self.label.x < -(self.width / 2 + self.label.texture_size[0] / 2):
+            self.label.x = self.width / 2 + self.label.texture_size[0] / 2
--- weather_station.py
+++ weather_station.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 from kivy import Logger
@@ -6,6 +7,7 @@ from kivy.uix.widget import Widget
 
 from config import Config
 from open_weather_api import OpenWeatherApi
+from open_weather_data import OpenWeatherData
 
 
 class WeatherStation(Widget):
@@ -31,8 +33,8 @@ class WeatherStation(Widget):
         Logger.info("update_weather: start")
 
         # -- for debugging --
-        # f = open("tests/json/test24.json", mode="r", encoding="utf-8")
-        # data = OpenWeatherData.from_dict(json.loads(f.read()))
+        #f = open("tests/json/test6.json", mode="r", encoding="utf-8")
+        #data = OpenWeatherData.from_dict(json.loads(f.read()))
         # -------------------
 
         data = self.open_weather_api.getdata()
@@ -41,7 +43,7 @@ class WeatherStation(Widget):
             self.ids.current_widget.weather_data = data
             self.ids.forecast_hourly_widget.weather_data = data
             self.ids.forecast_daily_widget.weather_data = data
-
+            self.ids.alert_widget.weather_data = data
         Logger.info("update_weather: end")
 
     def update_time(self, dt):
